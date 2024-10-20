d91818a1424f72b0e25c1027254e558aaaffb537
Moksh Mahajan <mmahajan2_be22@thapar.edu>
2024-10-20 15:44:26 +0530

    Input.cpp

---
+++ C++/input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // To take input from user
+    char name[50];
+    cout << "Enter your Name " << endl;
+    cin >> name;
+    cout << "Your name is " << name << endl;
+    return 0;
+}
